f04492b8b5f3d6292ce7b340c4cc9340ac48f7ae
Nahashon munyiri mutahi <nahashonmutahi95@gmail.com>
2026-07-05 20:38:21 +0300

    Change project name variable to 'mutahi tech'

--- new-project.sh
+++ new-project.sh
@@ -3,21 +3,20 @@
 
 PROJECT_NAME=$1
 
-if [ -z "$PROJECT_NAME" ]; then
+if [ -z "mutahi tech" ]; then
     echo "Usage: ./new-project.sh project-name"
     exit 1
 fi
 
-if [ -d "$PROJECT_NAME" ]; then
-    echo "Error: '$PROJECT_NAME' already exists."
+if [ -d "$mutahi tech" ]; then
+    echo "Error: '$mutahi tech' already exists."
     exit 1
 fi
 
-mkdir -p "$PROJECT_NAME"/{src/{css,js,images},docs,tests}
-touch "$PROJECT_NAME"/README.md
-touch "$PROJECT_NAME"/src/index.html
-touch "$PROJECT_NAME"/src/css/styles.css
-touch "$PROJECT_NAME"/src/js/main.js
+mkdir -p "$mutahi tech"/{src/{css,js,images},docs,tests}
+touch "$mutahi tech"/README.md
+touch "$mutahi tech"/src/index.html
+touch "$mutahi tech"/src/css/styles.css
+touch "$mutahi tech"/src/js/main.js
 
-echo "# $PROJECT_NAME" > "$PROJECT_NAME"/README.md
-echo "Project $PROJECT_NAME created successfully!"
+
